d5c007a9615d76f6d74b8740d223bd06fd407231
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2024-12-09 14:37:39 +0100

    authentication

--- routes/utilisateur.js
+++ routes/utilisateur.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const connexion = require("../config/bdd");
 
