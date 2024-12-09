33d43a85acd44e0ed23390b377807fbfaae0baa6
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2024-12-09 09:12:55 +0100

    route contact

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/api/contact', );
+
 
 
 app.listen(3001), () => {
---
+++ routes/CrudContact.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const bdd = require('../config/bdd');
+
+
+// +---------------+---------+
+// | idEtatMessage | libelle |
+// +---------------+---------+
+// |             1 | Nouveau |
+// |             2 | Archive |
+// +---------------+---------+
+
+
+// Route pour récuéperer tout les messages 
+router.get ('/AllMessages', (req, res) => {
+    const allMessages = "select * from contact"
+    bdd.query(allMessages, (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+    })
+// route pour récupérer tout les nouveaux messages
+router.get ('/NouveauxMessages', (req, res) => {
+    const newMessages = "select * from contact WHERE idEtat = 1"
+    bdd.query(newMessages, (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+    
+})
+// route pour récupérer tout les messages archivés 
+
+router.get ('/MessagesArchivés', (req, res) => {
+    const archivedMessages = "select * from contact WHERE idEtat = 2"
+    bdd.query(archivedMessages, (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+})
+
+// route pour créer un message 
+
+router.post ('/Messages', (req, res) => {
+    const postMessage = "insert into contact (nom, mail, message, idEtatMessage) values (?,?,?,1)"
+    bdd.query(postMessage, [req.body.nom, req.body.mail, req.body.message], (err, result) => {
+        if(err) throw err;
+        res.json({message: 'Message envoyé avec succès'});
+
+    })})
