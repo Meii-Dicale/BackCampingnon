98c582cca35ee7ef447628e46fb70c3ad12fadab
Meii-Dicale <meii7@hotmail.fr>
2024-12-12 09:38:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const routeUtilisateur = require("./routes/utilisateur");
 const CrudContact = require("./routes/CrudContact");
 const CrudReservation = require("./routes/CrudReservation");
 const crudEmplacement = require('./routes/crudEmplacement');
-const crudPromotion = require('./routes/CrudPromotion');
-
 const loginroute = require("./routes/Login");
 const CrudService = require('./routes/CrudServices');
 const cors = require('cors');
@@ -15,14 +13,7 @@ app.use(cors({
     origin: 'http://localhost:5173', // Autorise uniquement cette origine
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Liste des méthodes autorisées
     allowedHeaders: ['Content-Type', 'Authorization'], // Liste des en-têtes autorisés
-
   }));
-
-app.options('*', cors({ 
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-}));
 
 app.use(express.json());
 
@@ -30,8 +21,6 @@ app.use("/api/utilisateur", routeUtilisateur);
 app.use('/api/emplacement', crudEmplacement);
 app.use("/api/contact", CrudContact);
 app.use("/api/reservations", CrudReservation);
-app.use("/api/promotions", crudPromotion);
-
 app.use("/api/login" , loginroute);
 app.use("/api/services", CrudService);
 
