5a54e413d8294b724ae5db8ab9bdc9b41250a607
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2024-12-19 10:10:04 +0100

    upload photo

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ const path = require('path');
 const crudFinance = require('./routes/crudFinance');
 const factureRoutes = require('./routes/Facture');
 const crudPromotions = require('./routes/CrudPromotion');
+const crudPhoto = require('./routes/CrudPhoto');
 
 const EventEmitter = require('events');
 EventEmitter.defaultMaxListeners = 20; // Ajustez le nombre en fonction de vos besoins
 
-
-
-
 app.use(cors({
-    origin: 'http://localhost:5173', // Autorise uniquement cette origine
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Liste des méthodes autorisées
-    allowedHeaders: ['Content-Type', 'Authorization'], // Liste des en-têtes autorisés
-  }));
-  app.options('*', cors({ 
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-  }));
+  origin: 'http://localhost:5173', // Autorise uniquement cette origine
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Liste des méthodes autorisées
+  allowedHeaders: ['Content-Type', 'Authorization'], // Liste des en-têtes autorisés
+}));
+app.options('*', cors({ 
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
+
 
 app.use(express.json());
+
+
 
 app.use("/api/utilisateur", routeUtilisateur);
 app.use("/api/emplacement", crudEmplacement);
@@ -40,10 +42,13 @@ app.use("/api/services", CrudService);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')), crudEmplacement);
 app.use('/api/finances', crudFinance)
 app.use('/api/promotion', crudPromotions)
-
 app.use('/api/facture', factureRoutes);
+app.use('/api/photo', crudPhoto)
 
 const PORT = 3001;
+
+
+
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
--- routes/crudFinance.js
+++ routes/crudFinance.js
@@ -50,4 +50,37 @@ router.get('/emplacements',authenticateToken ,(req, res) => {
   });
 });
 
+// API pour obtenir le motant total des emplacements , le montant total des services associé et des services de reservations à la date du jour 
+router.get('/totalDuJour', (req, res) => {
+  const today = new Date();
+  const query = "SELECT SUM(e.tarif + IFNULL(s.tarif, 0)) AS somme_tarifs FROM reservation r JOIN emplacement e ON r.idEmplacement = e.idEmplacement LEFT JOIN serviceReservation sr ON r.idReservation = sr.idReservation LEFT JOIN service s ON sr.idService = s.idService WHERE r.dateEntree <= CURDATE() AND r.dateSortie >= CURDATE()"
+
+  bdd.query(query, [today, today], (err, results) => {
+    if (err) throw err;
+    res.json(results[0]);
+  });
+});
+router.get('/totalDeLaSemaine', (req, res) => {
+  const query = "SELECT SUM(e.tarif + IFNULL(s.tarif, 0)) AS somme_tarifs FROM reservation r JOIN emplacement e ON r.idEmplacement = e.idEmplacement LEFT JOIN serviceReservation sr ON r.idReservation = sr.idReservation LEFT JOIN service s ON sr.idService = s.idService WHERE YEARWEEK(r.dateEntree, 1) = YEARWEEK(CURDATE(), 1) OR YEARWEEK(r.dateSortie, 1) = YEARWEEK(CURDATE(), 1);"
+  
+  bdd.query(query, (err, results) => {
+    if (err) throw err;
+    res.json(results[0]);
+  });
+});
+
+
+
+
+router.get('/totalDuMois', (req, res) => {
+
+  const query = "SELECT SUM(e.tarif + IFNULL(s.tarif, 0)) AS somme_tarifs FROM reservation r JOIN emplacement e ON r.idEmplacement = e.idEmplacement LEFT JOIN serviceReservation sr ON r.idReservation = sr.idReservation LEFT JOIN service s ON sr.idService = s.idService WHERE MONTH(r.dateEntree) = MONTH(CURDATE()) AND YEAR(r.dateEntree) = YEAR(CURDATE()) OR MONTH(r.dateSortie) = MONTH(CURDATE()) AND YEAR(r.dateSortie) = YEAR(CURDATE());"
+  bdd.query(query, (err, results) => {
+    if (err) throw err;
+    res.json(results[0]);
+  });
+});
+
+
+
 module.exports = router
